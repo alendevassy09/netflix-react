0d847d20ba697ed424b1e9cb6df14949b93867df
alen devassy <alendevassy09@gmail.com>
2022-09-20 17:00:53 +0530

    extra movie row added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Banner from "./Components/Banner/banner";
 import NavBar from "./Components/NavBar/NavBar";
 import RowPost from "./Components/RowPost/RowPost";
-import {originals,action} from './urls'
+import {originals,action, comedy, horror} from './urls'
 import './App.css'
 
 function App() {
@@ -12,6 +12,8 @@ function App() {
       <Banner/>
       <RowPost  url={originals} title='Netflix Originals'/>
       <RowPost  url={action} title='Action Movies' isSmall/>
+      <RowPost  url={comedy} title='Comedy Movies' isSmall/>
+      <RowPost  url={horror} title='Horror Movies' isSmall/>
     </div>
   );
 }
--- src/urls.js
+++ src/urls.js
@@ -4,3 +4,7 @@ import { apiKey } from "./constants/constants";
 export const originals=`discover/tv?api_key=${apiKey}&with_networks=213`
 
 export const action=`discover/movie?api_key=${apiKey}&with_genres=28`
+
+export const comedy=`discover/movie?api_key=${apiKey}&with_genres=35`
+
+export const horror=`discover/movie?api_key=${apiKey}&with_genres=27`
